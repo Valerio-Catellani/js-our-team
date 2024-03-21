7d12b2d529c88fb812f32bd5fd54f579974e2451
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-21 16:03:23 +0100

    added the fnction to add a new member

--- resources/js/hypeUtility.js
+++ resources/js/hypeUtility.js
@@ -29,3 +29,34 @@ function addClass(element, value) {
         element.classList.add(value)
     }
 }
+
+
+//^ FUNCTION MODAL END
+function hypeModal(score = "", condition = "") {
+
+    const BackGroundBlack = document.createElement('div');
+    BackGroundBlack.id = "hype-modal"
+    BackGroundBlack.className = "position-absolute w-100 h-100 overflow-hidden d-flex align-items-center justify-conentent-center position-fixed";
+    BackGroundBlack.style = "z-index:100; left:0; top:0; background-color: rgba(0, 0, 0, 0.4);";
+
+    const EndBanner = document.createElement('div');
+    EndBanner.className = "mx-auto d-flex align-items-center justify-content-center flex-column";
+    EndBanner.style = "width:700px; height:700px;";
+    EndBanner.id = `banner`
+
+    const EndText = document.createElement('h2');
+    EndText.className = "text-white text-center";
+    EndText.style = "font-size:5rem; -webkit-text-stroke: 1px black; ";
+    EndText.innerHTML = `You found ${score}/${condition}! <p class='fs-1'>Total Points: ${score}</p>`;
+
+    const EndButton = document.createElement('button')
+    EndButton.id = "end"
+    EndButton.className = `btn btn-lg bg-${score !== 5 ? "danger" : "success"}`
+    EndButton.innerHTML = score !== 5 ? `Try Again` : "Play Again!"
+    EndButton.addEventListener('click', () => {
+        document.getElementById("hype-modal").remove()
+    })
+    EndBanner.append(EndText, EndButton)
+    BackGroundBlack.appendChild(EndBanner);
+    return BackGroundBlack
+}
--- resources/js/script.js
+++ resources/js/script.js
@@ -72,27 +72,32 @@ const teamMembers = [
     }
 ];
 
-app()
 
+
+//& MAIN APP
+app()
 function app() {
     const CardContianer = document.createElement('div')
     CardContianer.id = "card-container";
-    CardContianer.className = "d-flex flex-wrap align-items-center justify-content-around"
+    CardContianer.className = "d-flex flex-wrap align-items-center justify-content-center"
     for (let element of teamMembers) {
         let userImg = `/resources/img/${element.userImg}`
         CardContianer.append(createCard(element.name, element.surname, element.role, userImg))
     }
-    CardContianer.append(createCard("Add a new Member"))
+    CardContianer.append(createAddCard())
     document.getElementById('app').append(CardContianer);
 
 }
 
 
 
+
+
+//^FUNCTION createCard
 function createCard(userName, userSurname = "", userRole = "", userImgUrl = "") {
     const Card = document.createElement("div");
-    Card.className = "card m-3";
-    Card.style = "width:30%; min-width:400px";
+    Card.className = "card m-3 border-3";
+    Card.style = "min-width:300px; width:30%";
     const CardImg = document.createElement("img");
     CardImg.className = "img-fluid bg-"
     CardImg.setAttribute("src", `${userImgUrl}`);
@@ -109,4 +114,81 @@ function createCard(userName, userSurname = "", userRole = "", userImgUrl = "") 
     Card.append(CardImg, CardBody)
     return Card;
 }
+
+//^FUNCTION: createAddCard
+function createAddCard() {
+    const Card = document.createElement("div");
+    Card.className = "card m-3 border-3";
+    Card.id = "add-member";
+    Card.style = "min-width:300px; width:30%; cursor:pointer";
+    const CardImg = document.createElement("div");
+    CardImg.className = "bg-dark-subtle d-flex align-items-center justify-content-center display-1 text-white"
+    CardImg.innerHTML = '<i class="fa-solid fa-user-plus"></i>'
+    CardImg.style = "height:380px"
+    const CardBody = document.createElement('div');
+    CardBody.className = "card-body text-center";
+    const CardTitle = document.createElement('h5');
+    CardTitle.className = "card-title fs-1";
+    CardTitle.innerHTML = `Add a new Member`;
+    CardBody.append(CardTitle);
+    Card.append(CardImg, CardBody)
+    Card.addEventListener('click', () => document.getElementById('app').append(addUser()))
+    return Card;
+}
+
+//^FUNCTION: addUser
+
+function addUser() {
+    //--- background
+    const BackGroundBlack = document.createElement('div');
+    BackGroundBlack.id = "hype-modal"
+    BackGroundBlack.className = "position-absolute w-100 h-100 overflow-hidden d-flex align-items-center justify-conentent-center position-fixed";
+    BackGroundBlack.style = "z-index:100; left:0; top:0; background-color: rgba(0, 0, 0, 0.4);";
+    //--- Banner
+    const Banner = document.createElement('div');
+    Banner.className = "mx-auto d-flex align-items-center justify-content-center flex-column bg-light border border-dark border-3 rounded-4";
+    Banner.style = "width:700px; height:700px;";
+    Banner.id = `banner`
+    //--- Title
+    const Title = document.createElement("h2");
+    Title.innerHTML = "Add a new Member";
+    Banner.append(Title)
+    //--- Information field
+    for (let element in teamMembers[0]) {
+        Banner.append(generateInformation(element))
+    }
+    //--- SendButton
+    const SendButton = document.createElement('button')
+    SendButton.id = "end"
+    SendButton.className = `btn btn-lg`
+    SendButton.innerHTML = "Play Again!"
+    SendButton.addEventListener('click', () => {
+        document.getElementById("hype-modal").remove()
+    })
+    Banner.append(SendButton)
+    BackGroundBlack.appendChild(Banner);
+    return BackGroundBlack
+}
+
+
+
+
+function generateInformation(key) {
+    //--- container (label + input)
+    const container = document.createElement('div');
+    container.className = "my-3 answer-container w-75";
+    //--- 
+    const label = document.createElement('label');
+    label.htmlFor = `user-input-${key}`;
+    label.className = "form-label";
+    label.innerHTML = `Insert the ${key} of the new member`
+    const input = document.createElement('input');
+
+    input.id = `user-input-${key}`;
+    input.className = "user-input-value form-control border-danger bg-danger-subtle border-4";
+    input.setAttribute("type", key !== "userImg" ? "text" : "file");
+    container.append(label, input);
+    return container
+}
+
 
